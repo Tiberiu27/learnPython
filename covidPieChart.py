b5136ef757d1016d14c59338ce75bc5544c00027
Tiberiu27 <grigoras.tiberiu@gmail.com>
2020-10-20 23:15:30 +0300

    Add files via upload

--- covidPieChart.py
+++ covidPieChart.py
@@ -3,12 +3,18 @@
 import json, requests, openpyxl, os, sys
 import pyinputplus as pyip
 
+
 #Create a folder where we'll put the file with data
 abspath = os.path.abspath(sys.argv[0])
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 os.makedirs('covidData', exist_ok=True)
 
+#Create the excel file
+wb = openpyxl.Workbook()
+sheet = wb.active
+counties = []
+cases = []
 
 #Get the JSON data
 url = 'https://www.graphs.ro/json.php'
@@ -21,7 +27,43 @@ w = json.loads(res.text)
 
 #Select the date you're interested in
 
-userDate = str(pyip.inputDate(prompt= 'Enter a date (yyyy-mm-dd) format: ',formats= ['%Y-%m-%d']))
+userDate = pyip.inputDate(prompt= 'Enter a date (yyyy-mm-dd) format: ',formats= ['%Y-%m-%d'])
+userDate = str(userDate)
+year, month, day = userDate.replace(' ', '').split('-')
+
+userCounty = pyip.inputStr(prompt= 'You are interested in a specific county? ')
+userCounty = userCounty.lower()
+userCounty = userCounty[0].upper() + userCounty[1:]
+
+#Creating a list with all the county names from the data
+countyDict = w['covid_romania'][0]['county_data']
+totalCounties = []
+for i in countyDict:
+    totalCounties.append(i['county_name'].lower())
+
+def timeWalking():
+    intDay = int(day)
+    intMonth = int(month)
+    intYear = int(year)
+    days = []
+
+    for i in range(14):
+        if (intDay == 0 and intMonth == 10) or (intDay == 0 and intMonth == 12) or (intDay == 0 and intMonth == 5) or (intDay == 0 and intMonth == 7):
+            intDay = 30
+            intMonth -= 1
+        elif intDay == 0 and intMonth == 3:
+            if intYear % 4 == 0:
+                intDay =2
+                intMonth -= 1
+            else:
+                intDay = 28
+                intMonth -= 1
+        elif intDay == 0:
+            intDay = 31
+            intMonth -= 1
+        days.append(year + '-' + str(intMonth).rjust(2, '0')+ '-' + str(intDay).rjust(2,'0')) #the rjust(2, '0') puts a 0 on the left if the value is single digit.
+        intDay -= 1
+    return days
 
 def daySelector():
     for i in w['covid_romania']:
@@ -31,18 +73,49 @@ def daySelector():
                 cases = i['new_cases_today']
                 tests = i['new_tests_today']
                 print(str(cases) + ' cases confirmed from '+ str(tests) + ' tests, that means ' + str(round(cases / (tests / 100),1)) + '%') 
-                return dayData
+                return dayData['county_data']
+
     
+def timelineSelector():
+    cases = {}
+    timeline = timeWalking()
+    for i in w['covid_romania']:
+        for v in i.values():
+            if v in timeline:
+                dayData = i
+                day = i['reporting_date']
+                countyList = i['county_data']
+                for j in countyList:
+                    if j['county_name'] == userCounty:
+                        cases[v] = j['total_cases']
+    return cases 
 
-#Create the excel file
-wb = openpyxl.Workbook()
-sheet = wb.active
-counties = []
-cases = []
 
+if userCounty.lower() in totalCounties:
+    timeCases = timelineSelector()
+    countySheet = wb.create_sheet(userCounty + 'Sheet')
+    i = 1
+    for k, v in sorted(timeCases.items()):
+        countySheet.cell(row= i, column= 1).value = k
+        countySheet.cell(row= i, column= 2).value = v
+        i = i + 1
+    
+    countyChart = openpyxl.chart.LineChart()
+    countyChart.style = 12
+    countyChart.title = 'Evolution cases'
+    countyChart.x_axis.title = 'Date'
+    countyChart.y_axis.title = 'Evolution'
+
+    countyData = openpyxl.chart.Reference(countySheet, min_col= 2, min_row= 1, max_row = countySheet.max_row, max_col= 2)
+    countyDates = openpyxl.chart.Reference(countySheet, min_col= 1, min_row= 1, max_row= countySheet.max_row)
+    countyChart.add_data(countyData, titles_from_data= False)
+    countyChart.set_categories(countyDates)
+    countyChart.legend = None
+    countySheet.add_chart(countyChart, 'E5')
+    
 data = daySelector()
 try:
-    for county in data['county_data']:
+    for county in data:
         counties.append(county['county_name'])
         cases.append(county['total_cases'])
 except:
@@ -66,5 +139,6 @@ pieChart.height = 12
 pieChart.width = 15
 sheet.add_chart(pieChart, 'E5')
 wb.save(os.path.join('covidData', str(userDate + '.xlsx'))) # the file will be named after the date.
+
 
 
